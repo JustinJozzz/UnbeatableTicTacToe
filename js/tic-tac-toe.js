1b9913ab9ddd0f75c0fc3c629c732ad39a9d340d
Justin Josey <jdjst297@mail.rmu.edu>
2019-10-04 22:14:44 -0400

    Added win checking

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -27,6 +27,41 @@ function Game (player1, player2) {
     this.checkAvailable = function (move) {
         return _this.player1.moves.indexOf(move) === -1 && _this.player2.moves.indexOf(move) === -1;
     }
+
+    this.checkWin = function () {
+        if (_this.turnPlayer.moves.length >= 3) {
+            var diag1 = [];
+            var diag2 = [];
+
+            for (var i = 0; i <= 2; i++) {
+                var row = _this.turnPlayer.moves.filter(function(x) {
+                   return x[0] === i;
+                });
+
+                var col = _this.turnPlayer.moves.filter(function(x) {
+                    return x[1] === i;
+                });
+
+                if (row.length === 3 || col.length === 3) {
+                    return true;
+                }
+
+                if (_this.turnPlayer.moves.filter(function(x) { return x[0] === i && x[1] === i; }).length > 0) {
+                    diag1.push(_this.turnPlayer.moves.filter(function(x) { return x[0] === i && x[1] === i; })[0]);
+                }
+
+                if (_this.turnPlayer.moves.filter(function(x) { return x[0] === i && x[1] === Math.abs(i-2); }).length > 0) {
+                    diag2.push(_this.turnPlayer.moves.filter(function(x) { return x[0] === i && x[1] === Math.abs(i-2); })[0]);
+                }
+            }
+
+            if (diag1.length === 3 || diag2.length === 3) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
 
 var player1 = new Player('close', 1);
@@ -37,13 +72,20 @@ $(function() {
     $('.tic-tac-cell').click(function() {
         var move = $(this).data('pos');
 
-        if (game.checkAvailable(move)) {
+        if (game.state === 'play' && game.checkAvailable(move)) {
             game.takeTurn(move);
             $(this).html('<i class="material-icons player-move">' + game.turnPlayer.icon + '</i>');
+
+            if (game.checkWin()) {
+                game.state = 'win';
+                console.log(game.turnPlayer + ' wins!');
+            }
 
             if (game.state === 'play') {
                 game.nextTurn();
             }
+        } else {
+            console.log('Space Already Taken');
         }
     });
 });
